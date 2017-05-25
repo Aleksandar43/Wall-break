93d9fe54c6f2bbc2a6e290310cb38a3d46828e53
Aleksandar43 <a.plahcinski@gmail.com>
2017-05-25 03:01:56 +0200

    Dodati zidovi i funkcija napraviOblik Zidovi se ne slažu sa dimenzijama prozora koje su za 10 veće od zadatih dimenzija

--- src/računarskagrafika/domaći/rušenjezida/Blok.java
+++ src/računarskagrafika/domaći/rušenjezida/Blok.java
@@ -1,21 +1,26 @@
 package računarskagrafika.domaći.rušenjezida;
 
+import javafx.scene.paint.Color;
 import javafx.scene.paint.Paint;
 import javafx.scene.shape.Rectangle;
 
 public class Blok extends Sprite implements Odbijajući{
     private Rectangle oblik;
     public Blok(double x, double y, double širina, double visina, Paint boja) {
-        oblik=new Rectangle(x, y, širina, visina);
-        oblik.setFill(boja);
-        getChildren().add(oblik);
+        napraviOblik(x, y, širina, visina, boja);
     }
 
     public Blok(double x, double y, double širina, double visina) {
-        oblik=new Rectangle(x, y, širina, visina);
-        getChildren().add(oblik);
+        napraviOblik(x, y, širina, visina, Color.GRAY);
     }
 
+    private void napraviOblik(double x, double y, double širina, double visina, Paint boja){
+        oblik=new Rectangle(x, y, širina, visina);
+        oblik.setFill(boja);
+        oblik.setStroke(Color.BLACK);
+        getChildren().add(oblik);
+    }
+    
     @Override
     public void odbijanje() {
         //blok treba da nestane
--- src/računarskagrafika/domaći/rušenjezida/RušenjeZida.java
+++ src/računarskagrafika/domaći/rušenjezida/RušenjeZida.java
@@ -16,7 +16,9 @@ public class RušenjeZida extends Application {
     public static final int BROJ_BLOKOVA_U_REDU=13,
             BROJ_BLOKOVA_U_KOLONI=6;
     public static final double ŠIRINA_BLOKA=30,
-            VISINA_BLOKA=20;
+            VISINA_BLOKA=20,
+            ŠIRINA_PROZORA=480,
+            VISINA_PROZORA=640;
     public static final Color[] BOJE={Color.GREY,Color.PURPLE, Color.BLUE, Color.GREEN, Color.YELLOW, Color.RED};
     
     @Override
@@ -25,6 +27,11 @@ public class RušenjeZida extends Application {
         
         ImagePattern p=new ImagePattern(new Image("crteži/bg.png"), 0, 0, 40, 40, false);
         
+        Zid leviZid=new Zid(0, 0, 10, VISINA_PROZORA, Color.GRAY),
+                desniZid=new Zid(ŠIRINA_PROZORA-10, 0, 10, VISINA_PROZORA, Color.GRAY),
+                gornjiZid=new Zid(0, 0, ŠIRINA_PROZORA, 10, Color.GRAY);
+        glavnaGrupa.getChildren().addAll(leviZid, desniZid, gornjiZid);
+        
         Blok[] blokovi = new Blok[BROJ_BLOKOVA_U_REDU*BROJ_BLOKOVA_U_KOLONI];
         for(int i=0;i<blokovi.length;i++){
             blokovi[i]=new Blok((i%BROJ_BLOKOVA_U_REDU)*ŠIRINA_BLOKA, (i/BROJ_BLOKOVA_U_REDU)*VISINA_BLOKA,
@@ -32,7 +39,7 @@ public class RušenjeZida extends Application {
             glavnaGrupa.getChildren().add(blokovi[i]);
         }
         
-        Scene scene = new Scene(glavnaGrupa, 480, 640);
+        Scene scene = new Scene(glavnaGrupa, ŠIRINA_PROZORA, VISINA_PROZORA);
         scene.setFill(p);
         
         primaryStage.setTitle("Rušenje zida");
